f87112ca2a4a950df990a65d5905b7f5a382e5b4
Khaled Nassar <knassar702@gmail.com>
2026-01-13 11:54:15 +0200

    t

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 7070,
-    strictPort: true,
-    hmr: {
-      clientPort: 7070,
-    },
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: 7070,
-  },
 });
